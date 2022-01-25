5187dd595acac2dbde45f6175506d03591bf783c
rikzabk <rikzabk@gmail.com>
2022-01-25 15:09:17 +0700

    commit first dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// route: admin
+Route::get('/admin', function () {
+    return view('layout.index');
+});
+// end route: admin
