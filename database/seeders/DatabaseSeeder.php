f13c8e90f25edee12b09ddb83e7bd3ac37eef49a
rikzabk <rikzabk@gmail.com>
2022-04-13 15:07:07 +0700

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        Partner::factory(30)->create();
+        // Partner::factory(30)->create();
         Careerfair::create([
             'title' => 'AOCF 35th Unair',
             'description' => '<p>Airlangga Online Career Fair (AOCF) 2022 merupakan salah satu wahana strategis bagi kepentingan perusahaan/corporation dan jobseeker. Pada tahun 2022 ini, Airlangga Online Career Fair (AOCF) diadakan secara online/virtual melalui web DPKKA mengingat kondisi saat ini yang masih dalam pandemic COVID-19. Dalam hal ini, Corporation memiliki kemudahan untuk mencari talent yang sesuai dengan kualifikasi yang dibutuhkan untuk mempercepat growth dan pengembangan bisnis utamanya.</p><p>   Sedangkan bagi jobseeker, dalam event AOCF ini dapat dijadikan peluang dalam melamar pekerjaan ditengah kondisi pandemic COVID-19 terhadap jenis perusahaan dan bidang yang menjadi passion personalnya.</p><p>   Selain itu, AOCF 2022 terbuka untuk umum. Tidak hanya lulusan dari Universitas Airlangga, tetapi juga berbagai lulusan dari perguruan tinggi lain baik negeri maupun swasta. Dengan demikian, kegiatan ini sangat strategis untuk dijadikan agenda utama dalam berpartisipasi secara aktif.</p><p><br><br>&nbsp;</p><h4><strong>Rangkaian kegiatan Airlangga Online Career Fair (AOCF) 2022 :</strong></h4><ul><li>Publikasi Informasi Lowongan Pekerjaan perusahaan mitra DPKKA</li><li>Seminar online oleh mitra DPKKA</li></ul>',
