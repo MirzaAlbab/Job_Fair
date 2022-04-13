02b87207cf83a028f55a37c39add6cc1c97847e7
rikzabk <rikzabk@gmail.com>
2022-04-13 14:44:11 +0700

    partisipan seeder

--- database/seeders/PartnerSeeder.php
+++ database/seeders/PartnerSeeder.php
@@ -15,12 +15,74 @@ class PartnerSeeder extends Seeder
     public function run()
     {
         Partner::create([
-            'company' => 'perusahaan satu',
-            'description' => 'perusahaan ke-satu',
+            'company' => 'BRI',
+            'description' => 'BRI',
             'careerfair_id' => 1,
-            'position' => 'top',
-            'img' => 'halo',
-            'status' => 'inactive'
+            'position' => '1',
+            'status' => 'active'
+        ]);
+        Partner::create([
+            'company' => 'Mandiri',
+            'description' => 'Mandiri',
+            'careerfair_id' => 1,
+            'position' => '1',
+            'status' => 'active'
+        ]);
+        Partner::create([
+            'company' => 'Pegadaian',
+            'description' => 'Pegadaian',
+            'careerfair_id' => 1,
+            'position' => '1',
+            'status' => 'active'
+        ]);
+        Partner::create([
+            'company' => 'Mega Persada Group',
+            'description' => 'Mega Persada Group',
+            'careerfair_id' => 1,
+            'position' => '1',
+            'status' => 'active'
+        ]);
+        Partner::create([
+            'company' => 'TECHCONNECT academy',
+            'description' => 'TECHCONNECT academy',
+            'careerfair_id' => 1,
+            'position' => '1',
+            'status' => 'active'
+        ]);
+        Partner::create([
+            'company' => 'Malindo',
+            'description' => 'Malindo',
+            'careerfair_id' => 1,
+            'position' => '2',
+            'status' => 'active'
+        ]);
+        Partner::create([
+            'company' => 'AnterAja',
+            'description' => 'AnterAja',
+            'careerfair_id' => 1,
+            'position' => '2',
+            'status' => 'active'
+        ]);
+        Partner::create([
+            'company' => 'LOreal',
+            'description' => 'LOreal',
+            'careerfair_id' => 1,
+            'position' => '2',
+            'status' => 'active'
+        ]);
+        Partner::create([
+            'company' => 'Bank Muamalat',
+            'description' => 'Bank Muamalat',
+            'careerfair_id' => 1,
+            'position' => '2',
+            'status' => 'active'
+        ]);
+        Partner::create([
+            'company' => 'PT Graha Mutu Persada',
+            'description' => 'PT Graha Mutu Persada',
+            'careerfair_id' => 1,
+            'position' => '2',
+            'status' => 'active'
         ]);
     }
 }
