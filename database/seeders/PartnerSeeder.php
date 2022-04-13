7e35bdd141fd6b6a1751ffbd4d10812bada938f7
unknown <mirzaalbabfirdaus17@gmail.com>
2022-04-13 14:48:47 +0700

    update partner seeder image

--- database/seeders/PartnerSeeder.php
+++ database/seeders/PartnerSeeder.php
@@ -18,6 +18,7 @@ class PartnerSeeder extends Seeder
             'company' => 'BRI',
             'description' => 'BRI',
             'careerfair_id' => 1,
+            'img' => 'https://res.cloudinary.com/dxd813fbq/image/upload/v1649835947/CareerFair/2022-04-13_074547_bri.png',
             'position' => '1',
             'status' => 'active'
         ]);
@@ -25,6 +26,7 @@ class PartnerSeeder extends Seeder
             'company' => 'Mandiri',
             'description' => 'Mandiri',
             'careerfair_id' => 1,
+            'img' => 'https://res.cloudinary.com/dxd813fbq/image/upload/v1649835957/CareerFair/2022-04-13_074557_LOGO-Mandiri.png',
             'position' => '1',
             'status' => 'active'
         ]);
@@ -32,6 +34,7 @@ class PartnerSeeder extends Seeder
             'company' => 'Pegadaian',
             'description' => 'Pegadaian',
             'careerfair_id' => 1,
+            'img' => 'https://res.cloudinary.com/dxd813fbq/image/upload/v1649835968/CareerFair/2022-04-13_074608_LOGO-Pegadaian.png',
             'position' => '1',
             'status' => 'active'
         ]);
@@ -39,6 +42,7 @@ class PartnerSeeder extends Seeder
             'company' => 'Mega Persada Group',
             'description' => 'Mega Persada Group',
             'careerfair_id' => 1,
+            'img' => 'https://res.cloudinary.com/dxd813fbq/image/upload/v1649835986/CareerFair/2022-04-13_074626_LOGO-Mega-Persada-Group.png',
             'position' => '1',
             'status' => 'active'
         ]);
@@ -46,6 +50,7 @@ class PartnerSeeder extends Seeder
             'company' => 'TECHCONNECT academy',
             'description' => 'TECHCONNECT academy',
             'careerfair_id' => 1,
+            'img' => 'https://res.cloudinary.com/dxd813fbq/image/upload/v1649835995/CareerFair/2022-04-13_074635_LOGO-Techconnect-Academy.png',
             'position' => '1',
             'status' => 'active'
         ]);
@@ -53,6 +58,7 @@ class PartnerSeeder extends Seeder
             'company' => 'Malindo',
             'description' => 'Malindo',
             'careerfair_id' => 1,
+            'img' => 'https://res.cloudinary.com/dxd813fbq/image/upload/v1649836011/CareerFair/2022-04-13_074651_LOGO-Malindo.png',
             'position' => '2',
             'status' => 'active'
         ]);
@@ -60,6 +66,7 @@ class PartnerSeeder extends Seeder
             'company' => 'AnterAja',
             'description' => 'AnterAja',
             'careerfair_id' => 1,
+            'img' => 'https://res.cloudinary.com/dxd813fbq/image/upload/v1649836034/CareerFair/2022-04-13_074714_LOGO-Anteraja.png',
             'position' => '2',
             'status' => 'active'
         ]);
@@ -67,6 +74,7 @@ class PartnerSeeder extends Seeder
             'company' => 'LOreal',
             'description' => 'LOreal',
             'careerfair_id' => 1,
+            'img' => 'https://res.cloudinary.com/dxd813fbq/image/upload/v1649836043/CareerFair/2022-04-13_074723_LOGO-Loreal.png',
             'position' => '2',
             'status' => 'active'
         ]);
@@ -74,6 +82,7 @@ class PartnerSeeder extends Seeder
             'company' => 'Bank Muamalat',
             'description' => 'Bank Muamalat',
             'careerfair_id' => 1,
+            'img' => 'https://res.cloudinary.com/dxd813fbq/image/upload/v1649836053/CareerFair/2022-04-13_074733_LOGO-Bank-Muamalat.png',
             'position' => '2',
             'status' => 'active'
         ]);
@@ -81,6 +90,7 @@ class PartnerSeeder extends Seeder
             'company' => 'PT Graha Mutu Persada',
             'description' => 'PT Graha Mutu Persada',
             'careerfair_id' => 1,
+            'img' => 'https://res.cloudinary.com/dxd813fbq/image/upload/v1649836065/CareerFair/2022-04-13_074745_LOGO-GMP.png',
             'position' => '2',
             'status' => 'active'
         ]);
